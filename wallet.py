197328db7a4020c3a8998060b25f9d1ef9bf988e
iratxurrutia <146710848+iratxurrutia@users.noreply.github.com>
2025-12-07 23:01:32 +0100

    Add method to increase balance by 1000

--- wallet.py
+++ wallet.py
@@ -10,3 +10,6 @@ class Wallet:
 
     def remove_balance(self, val):
         self.balance = self.balance - val
+
+    def increase_balance_in_1000(self):
+        self.balance = self.balance + 1000
